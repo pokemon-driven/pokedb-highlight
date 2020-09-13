bbc663dee53cadfde0415281d044ea347a1409c2
Takuma HANATANI <mail@potato4d.me>
2020-09-13 21:45:31 +0900

    feat: Start cron

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pokedb-highlight
+
+[![Image from Gyazo](https://i.gyazo.com/a3cdc6aca5c3cf0b23533515172d70e7.png)](https://gyazo.com/a3cdc6aca5c3cf0b23533515172d70e7)
+
+Pokemon usage ranking crawl bot for Slack/Discord
+
+https://swsh.pokedb.tokyo/pokemon/list
+
+## Usage
+
+1. Fork this repo
+2. Set environment variables
+
+## LICENCE
+
+MIT
--- index.ts
+++ index.ts
@@ -1,67 +1,88 @@
 import puppeteer from 'puppeteer'
-import { promises as fs } from 'fs'
 import dayjs from 'dayjs'
 import axios from 'axios'
 import FormData from 'form-data'
 
+const Environments = {
+  GYAZO_ACCESS_TOKEN: process.env.GYAZO_ACCESS_TOKEN,
+  SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
+} as const
+
 type UploadAPIResponse = {
-  "image_id" : string
-  "permalink_url": string
-  "thumb_url" : string
-  "url" : string
-  "type": string
+  image_id: string
+  permalink_url: string
+  thumb_url: string
+  url: string
+  type: string
 }
 
 async function run() {
+  if (!(Environments.GYAZO_ACCESS_TOKEN && Environments.SLACK_WEBHOOK_URL)) {
+    console.error('Missing environment variables')
+    return process.exit(1)
+  }
+
   const browser = await puppeteer.launch({
     defaultViewport: {
       width: 1440,
-      height: 910
+      height: 910,
     },
-    args: [
-      '--window-size=1440,1040'
-    ]
+    args: ['--window-size=1440,1040'],
   })
   const page = await browser.newPage()
   await page.goto('https://swsh.pokedb.tokyo/pokemon/list')
   const list = await Promise.all(
-    (new Array(10).fill(0).map(async (_, i) => {
-      const el = await page.$(`.content .column:nth-child(${i+1}) .pokemon-ranking-name`)
+    new Array(10).fill(0).map(async (_, i) => {
+      const el = await page.$(
+        `.content .column:nth-child(${i + 1}) .pokemon-ranking-name`
+      )
       const text = await el?.getProperty('textContent')
       const v = await text?.jsonValue()
       const name = `${v}`.replace(/ /g, '').replace(/\n/g, '')
       return {
-        order: i+1,
-        name
+        order: i + 1,
+        name,
       }
-    }))
+    })
   )
-  const t = `${dayjs().format('YYYY/MM/DD hh:mm')} 時点のポケモン使用率ランキング
+  const ss = await page.screenshot()
+  try {
+    const form = new FormData()
+    form.append('access_token', `${Environments.GYAZO_ACCESS_TOKEN}`)
+    form.append('imagedata', ss, {
+      filename: 'ss.png',
+      knownLength: ss.length,
+    })
+    const res = await axios.post<UploadAPIResponse>(
+      'https://upload.gyazo.com/api/upload',
+      form,
+      {
+        headers: form.getHeaders(),
+      }
+    )
+    console.log(res.status + ' ' + res.statusText)
+    const t = `${dayjs().format(
+      'YYYY/MM/DD HH:mm'
+    )} のシングルバトルポケモン使用率ランキング
 
 ${list.map((p) => `${p.order}. ${p.name}`).join('\n')}
 
-#pokedbhighlight
-data source: https://swsh.pokedb.tokyo/pokemon/list`
-  console.log(t)
-  const ss = await page.screenshot()
-  // await fs.writeFile('ss.png', ss)
-  try {
-    const form = new FormData()
-    form.append('access_token', `${process.env.GYAZO_ACCESS_TOKEN}`)
-    form.append('imagedata', ss, {
-      filename: 'ss.png',
-      contentType: 'image/png',
-      knownLength: ss.length
-    })
-    const res = await axios.post<UploadAPIResponse>('https://upload.gyazo.com/api/upload',
-    form,
-    {
-      headers: form.getHeaders()
-    }
+---
+
+データソース: https://swsh.pokedb.tokyo/pokemon/list
+${res.data.permalink_url.replace('gyazo', 'i.gyazo')}.png`
+    console.log(t)
+    await axios.post(
+      Environments.SLACK_WEBHOOK_URL,
+      {
+        text: t,
+      },
+      {
+        headers: {},
+      }
     )
-    console.log(res.data.permalink_url)
-  } catch(e) {
-    console.log(e)
+  } catch (e) {
+    console.log(e.response)
   }
   process.exit(0)
 }
